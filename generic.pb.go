a8c0423e3e695fedf393fbd73656f6da1a897cbc
Akshay <kumarsharmaa69@gmail.com>
2017-03-22 17:32:26 +0530

    Added method isExist

--- generic.pb.go
+++ generic.pb.go
@@ -82,7 +82,7 @@ const _ = grpc.SupportPackageIsVersion4
 // Client API for Generic service
 
 type GenericClient interface {
-	Printify(ctx context.Context, in *Request, opts ...grpc.CallOption) (*Response, error)
+	IsExists(ctx context.Context, in *Request, opts ...grpc.CallOption) (*Response, error)
 }
 
 type genericClient struct {
@@ -93,9 +93,9 @@ func NewGenericClient(cc *grpc.ClientConn) GenericClient {
 	return &genericClient{cc}
 }
 
-func (c *genericClient) Printify(ctx context.Context, in *Request, opts ...grpc.CallOption) (*Response, error) {
+func (c *genericClient) IsExists(ctx context.Context, in *Request, opts ...grpc.CallOption) (*Response, error) {
 	out := new(Response)
-	err := grpc.Invoke(ctx, "/GenericClient.Generic/Printify", in, out, c.cc, opts...)
+	err := grpc.Invoke(ctx, "/GenericClient.Generic/IsExists", in, out, c.cc, opts...)
 	if err != nil {
 		return nil, err
 	}
@@ -105,27 +105,27 @@ func (c *genericClient) Printify(ctx context.Context, in *Request, opts ...grpc.
 // Server API for Generic service
 
 type GenericServer interface {
-	Printify(context.Context, *Request) (*Response, error)
+	IsExists(context.Context, *Request) (*Response, error)
 }
 
 func RegisterGenericServer(s *grpc.Server, srv GenericServer) {
 	s.RegisterService(&_Generic_serviceDesc, srv)
 }
 
-func _Generic_Printify_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+func _Generic_IsExists_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
 	in := new(Request)
 	if err := dec(in); err != nil {
 		return nil, err
 	}
 	if interceptor == nil {
-		return srv.(GenericServer).Printify(ctx, in)
+		return srv.(GenericServer).IsExists(ctx, in)
 	}
 	info := &grpc.UnaryServerInfo{
 		Server:     srv,
-		FullMethod: "/GenericClient.Generic/Printify",
+		FullMethod: "/GenericClient.Generic/IsExists",
 	}
 	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(GenericServer).Printify(ctx, req.(*Request))
+		return srv.(GenericServer).IsExists(ctx, req.(*Request))
 	}
 	return interceptor(ctx, in, info, handler)
 }
@@ -135,8 +135,8 @@ var _Generic_serviceDesc = grpc.ServiceDesc{
 	HandlerType: (*GenericServer)(nil),
 	Methods: []grpc.MethodDesc{
 		{
-			MethodName: "Printify",
-			Handler:    _Generic_Printify_Handler,
+			MethodName: "IsExists",
+			Handler:    _Generic_IsExists_Handler,
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
@@ -152,8 +152,8 @@ var fileDescriptor0 = []byte{
 	0x32, 0x53, 0xf3, 0x4a, 0x94, 0x64, 0xb9, 0xd8, 0x83, 0x52, 0x0b, 0x4b, 0x53, 0x8b, 0x4b, 0x84,
 	0x84, 0xb8, 0x58, 0x92, 0xf2, 0x53, 0x2a, 0x25, 0x18, 0x15, 0x18, 0x35, 0x78, 0x82, 0xc0, 0x6c,
 	0x25, 0x39, 0x2e, 0x8e, 0xa0, 0xd4, 0xe2, 0x82, 0xfc, 0xbc, 0xe2, 0x54, 0x6c, 0xf2, 0x46, 0x1e,
-	0x5c, 0xec, 0x50, 0xf3, 0x84, 0x6c, 0xb9, 0x38, 0x02, 0x8a, 0x32, 0xf3, 0x4a, 0x32, 0xd3, 0x2a,
+	0x5c, 0xec, 0x50, 0xf3, 0x84, 0x6c, 0xb9, 0x38, 0x3c, 0x8b, 0x5d, 0x2b, 0x32, 0x8b, 0x4b, 0x8a,
 	0x85, 0xc4, 0xf4, 0x50, 0x6c, 0xd1, 0x83, 0x5a, 0x21, 0x25, 0x8e, 0x21, 0x0e, 0x31, 0x5b, 0x89,
-	0x21, 0x89, 0x0d, 0xec, 0x3c, 0x63, 0x40, 0x00, 0x00, 0x00, 0xff, 0xff, 0xdf, 0xe5, 0xd5, 0x42,
+	0x21, 0x89, 0x0d, 0xec, 0x3c, 0x63, 0x40, 0x00, 0x00, 0x00, 0xff, 0xff, 0x4d, 0xd6, 0x86, 0x82,
 	0xaf, 0x00, 0x00, 0x00,
 }
